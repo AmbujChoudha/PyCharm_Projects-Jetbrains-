2870359740dfc66d927e873b8c56be0831839e6f
AmbujChoudha <ambujchoudha5796@gmail.com>
2020-07-10 00:00:53 +0530

    Updated Stage1 code for Stage2

--- Smart Calculator/Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/Smart Calculator/task/calculator/calculator.py
@@ -1,5 +1,19 @@
-num = [int(x) for x in input().split()]
-total = 0
-for i in range(len(num)):
-    total = total + num[i]
-print(total)
+def add(numbers):
+    total = sum(numbers)
+    return total
+
+
+while True:
+    user_input = input().split()
+    length = len(user_input)
+
+    if '/exit' in user_input:
+        print('Bye!')
+        break
+    elif length == 2:
+        num = [int(x) for x in user_input]
+        print(add(num))
+    elif length == 1:
+        print(*user_input)
+    elif length == 0:
+        continue
